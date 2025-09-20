fa1f29022deee988c478f59d472024d1d96d1e32
Damir <damirmuhlisov2014@gmail.com>
2025-09-20 23:42:27 +0400

    Добавил паттерн для bold + italic текста

--- Tools/MarkdownTextExtensions/StringExtensions.cs
+++ Tools/MarkdownTextExtensions/StringExtensions.cs
@@ -6,6 +6,7 @@ public static class StringExtensions
 {
     private const string ParagraphSplitPattern = @"\n(?:\s*\n)+";
     private const string HeadersPattern = @"^(#{1,6})\s*(.+)$";
+    private const string BoldItalicTextPattern = @"\*\*\*(.+?)\*\*\*|___(.+?)___";
     private const string BoldTextPattern = @"\*\*(.+?)\*\*|__(.+?)__";
     private const string ItalicTextPattern = @"\*(.+?)\*|_(.+?)_";
 
@@ -23,19 +24,22 @@ public static class StringExtensions
         if (headerMatch.Success)
         {
             var headerLevel = headerMatch.Groups[1].Value.Length;
-            var headerText = headerMatch.Groups[2].Value;
-
-            headerText = Regex.Replace(headerText, BoldTextPattern, m => $"<strong>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</strong>");
-            headerText = Regex.Replace(headerText, ItalicTextPattern, m => $"<em>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</em>");
+            var headerText = ConvertStyledText(headerMatch.Groups[2].Value);
             
             return $"<h{headerLevel}>{headerText}</h{headerLevel}>";
         }
 
-        var html = paragraph;
-        html = Regex.Replace(html, BoldTextPattern, m => $"<strong>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</strong>");
-        html = Regex.Replace(html, ItalicTextPattern, m => $"<em>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</em>");
+        var html = ConvertStyledText(paragraph);
         html = html.Replace("\n", "</br>");
         return $"<p>{html}</p>";
+    }
+
+    private static string ConvertStyledText(string text)
+    {
+        text = Regex.Replace(text, BoldItalicTextPattern, m => $"<strong><em>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</em></strong>");
+        text = Regex.Replace(text, BoldTextPattern, m => $"<strong>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</strong>");
+        text = Regex.Replace(text, ItalicTextPattern, m => $"<em>{DecideBetweenMatchingGroups(m.Groups[1], m.Groups[2])}</em>");
+        return text;
     }
 
     private static string DecideBetweenMatchingGroups(params Group[] groups)
@@ -43,5 +47,5 @@ public static class StringExtensions
         foreach (var group in groups)
             if (group.Success) return group.Value;
         return string.Empty;
-    } 
+    }
 }
