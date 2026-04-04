da1a177eeb291256b337c84fc96f24b7ef7fd3f9
Damir <damirmuhlisov2014@gmail.com>
2026-04-05 00:35:00 +0400

    remove markdown text processing

--- Tools/MarkdownTextExtensions/EnumerableExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Tools.MarkdownTextExtensions;
-
-public static class EnumerableExtensions
-{
-    public static string ConcatHtmlCode(this IEnumerable<string> htmlParagraphs)
-    {
-        return string.Join(string.Empty, htmlParagraphs);
-    }
-}
