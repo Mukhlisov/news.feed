cd30c8744bc2af64211d3daf48b5f6d5d4bc778d
Damir <damirmuhlisov2014@gmail.com>
2026-04-11 00:12:59 +0400

    change max take limit

--- news.feed.models/Consts.cs
+++ news.feed.models/Consts.cs
@@ -3,7 +3,7 @@ namespace news.feed.models;
 public static class Consts
 {
     public const int DefaultNewsBatchSize = 10;
-    public const int MaxTake = 50;
+    public const int MaxTake = 7;
     public const int MaxSkip = int.MaxValue;
     public const int MaxNewsTitleLength = 150;
 }
